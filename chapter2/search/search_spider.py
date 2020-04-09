036ddfaa318c78b6d00a56e456cfb18d612c436d
michaelliu <841412988@qq.com>
2020-04-09 10:45:41 +0800

    修改了目录

---
+++ chapter2/search/search_spider.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+# @File:Segment.py
+# @Author: Michael.liu
+# @Date:2019/2/12
+
+from bs4 import BeautifulSoup
+import urllib.request
+import xml.etree.ElementTree as ET
+import configparser
+
