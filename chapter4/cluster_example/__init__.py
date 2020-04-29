04a55bd9acdfc6e03b42a18f96409c14d426f9fa
michaelliu <841412988@qq.com>
2020-04-29 19:25:11 +0800

    learn_ralated_lda

---
+++ chapter4/cluster_example/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+# @File:__init__.py.py
+# @Author: Michael.liu
+# @Date:2020/4/27 19:53
+# @Desc: this code is ....
---
+++ chapter4/segment_example/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+# @File:__init__.py.py
+# @Author: Michael.liu
+# @Date:2020/4/27 19:54
+# @Desc: 之前已经涉及到一些分词的理论，
+#        具体也可以参考我写《聊天机器人入门、进阶与实战》，这里主要讲解一些基本的使用和一些主流分词
