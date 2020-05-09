bd16ac91c7c3de6e78c6a2e0eda055105ebc61c5
michaelliu <841412988@qq.com>
2020-05-09 13:38:23 +0800

    bilstm-crf segmentation

--- chapter4/segment_example/preprocess.py
+++ chapter4/segment_example/preprocess.py
@@ -7,28 +7,48 @@
 import codecs
 import os
 
-def character_tagging(input_file_, output_file_):
-    input_data = codecs.open(input_file_, 'r', 'utf-8')
-    output_data = codecs.open(output_file_, 'w', 'utf-8')
+# def character_tagging(input_file_, output_file_):
+#     input_data = codecs.open(input_file_, 'r', 'utf-8')
+#     output_data = codecs.open(output_file_, 'w', 'utf-8')
+#     for line in input_data.readlines():
+#         # 移除字符串的头和尾的空格。
+#         word_list = line.strip().split()
+#         for word in word_list:
+#             words = word.split("/")
+#             word = words[0]
+#             if len(word) == 1:
+#                 if word == '。' or word == '？' or word == '！':
+#                     output_data.write("\n")
+#                 else:
+#                     output_data.write(word + "/S ")
+#             elif len(word) >= 2:
+#                 output_data.write(word[0] + "/B ")
+#                 for w in word[1: len(word) - 1]:
+#                     output_data.write(w + "/M ")
+#                 output_data.write(word[len(word) - 1] + "/E ")
+#         output_data.write("\n")
+#     input_data.close()
+#     output_data.close()
+
+def character_tagging(input_file, output_file):
+    input_data = codecs.open(input_file, 'r', 'utf-8')
+    output_data = codecs.open(output_file, 'w', 'utf-8')
     for line in input_data.readlines():
-        # 移除字符串的头和尾的空格。
         word_list = line.strip().split()
         for word in word_list:
-            words = word.split("/")
-            word = words[0]
             if len(word) == 1:
-                if word == '。' or word == '？' or word == '！':
-                    output_data.write("\n")
-                else:
-                    output_data.write(word + "/S ")
-            elif len(word) >= 2:
-                output_data.write(word[0] + "/B ")
-                for w in word[1: len(word) - 1]:
-                    output_data.write(w + "/M ")
-                output_data.write(word[len(word) - 1] + "/E ")
+                output_data.write(word + "\tS\n")
+            else:
+                output_data.write(word[0] + "\tB\n")
+                for w in word[1:len(word)-1]:
+                    output_data.write(w + "\tM\n")
+                output_data.write(word[len(word)-1] + "\tE\n")
         output_data.write("\n")
     input_data.close()
     output_data.close()
+
+
+
 
 if __name__=="__main__":
     print("......start......")
