021ed83e1fd431e9a81f470b708f7b9fba0d96b6
michael.liu <841412988@qq.com>
2020-12-17 10:08:39 +0800

    new crawl example

---
+++ chapter4/crawl_example/b_crawl.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time : 2020/12/17 10:07 
+# @Author : michael.liu 
+# @Version：V 0.1
+# @File : b_crawl.py
+# @desc :
+
+
+
+if __name__ =='__main__':
+    print("Hello World!")
