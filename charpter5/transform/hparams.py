5f1db187e3fc83cff07358c4e271b3983917e288
michael.liu <841412988@qq.com>
2021-12-30 14:38:55 +0800

    params

--- charpter5/transform/hparams.py
+++ charpter5/transform/hparams.py
@@ -21,5 +21,29 @@ class Hparams:
     parser.add_argument('eval1',default='amazon/eval1.txt',help ='evaluation segmented data')
     parser.add_argument('--eval2', default='amazon/eval2.txt', help="evaluation segmented data")
 
+    # test
+    parser.add_argument('--test1', default='amazon/test1_.txt', help="test segment")
+    parser.add_argument('--test2', default='amazon/test2_.txt', help="test segment")
+    parser.add_argument('--ckpt', default='model/', help="checkpoint file path")
+    parser.add_argument('--ckpt_pb', default='model_pb/', help="checkpoint file path")
 
+    parser.add_argument('--testdir', default="test/", help="test result dir")
+    parser.add_argument('--evaldir', default="eval/", help="evaluation dir")
 
+    # training scheme
+    parser.add_argument('--batch_size', default=64, type=int)  ### 128
+    parser.add_argument('--eval_batch_size', default=64, type=int)  ### 128
+    parser.add_argument('--test_batch_size', default=64, type=int)
+    parser.add_argument('--lr', default=0.0003, type=float, help="learning rate")  ### 0.0003
+    parser.add_argument('--warmup_steps', default=1000, type=int)  ### 4000
+    parser.add_argument('--num_epochs', default=15, type=int)  ### 20
+
+    # model params
+    parser.add_argument('--d_model', default=512, type=int, help="hidden dimension of encoder/decoder")  ### 512
+    parser.add_argument('--d_ff', default=1024, type=int, help="hidden dimension of feedforward layer")  ### 2048
+    parser.add_argument('--num_blocks', default=3, type=int, help="number of encoder/decoder blocks")  ### 6
+    parser.add_argument('--num_heads', default=4, type=int, help="number of attention heads")  ### 8
+    parser.add_argument('--maxlen1', default=50, type=int, help="maximum length of a source sequence")  ### 100
+    parser.add_argument('--maxlen2', default=50, type=int, help="maximum length of a target sequence")  ### 100
+    parser.add_argument('--dropout_rate', default=0.3, type=float)
+    parser.add_argument('--smoothing', default=0.1, type=float, help="label smoothing rate")
